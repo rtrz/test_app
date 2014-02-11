3bcc31c0cff4f66d03b51c53f3a2f92ef552d1d1
Ravi Trivedi <ravi.trivedi3@gmail.com>
2014-02-10 22:33:57 -0800

    Add menu callbacks.

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -13,11 +14,28 @@ public class MainActivity extends Activity {
     }
 
 
+    // Place menu items into the action bar. Callback method to inflate the
+    // menu resource into the given Menu object.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
     
+    // Callback response to action buttons.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	// Handle presses on action bar items.
+    	switch (item.getItemId()) {
+    		case R.id.action_droid:
+    			//openDroid();
+    			return true;
+    		case R.id.action_settings:
+    			//openSettings();
+    			return true;
+    		default:
+    			return super.onOptionsItemSelected(item);
+    	}
+    }
 }
