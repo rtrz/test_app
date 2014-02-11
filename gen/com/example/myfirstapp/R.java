5d9d3b03cf9f2ed1e53ab6b0e64ea343f67ea3b8
Ravi Trivedi <ravi.trivedi3@gmail.com>
2014-02-11 01:18:01 -0800

    Read and write a list of timestamps from file.

--- gen/com/example/myfirstapp/R.java
+++ gen/com/example/myfirstapp/R.java
@@ -24,9 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_droid=0x7f080003;
-        public static final int action_settings=0x7f080002;
+        public static final int action_droid=0x7f080004;
+        public static final int action_settings=0x7f080003;
         public static final int intent_message=0x7f080001;
+        public static final int timestamp_history=0x7f080002;
         public static final int welcome_message=0x7f080000;
     }
     public static final class layout {
@@ -45,6 +46,7 @@ public final class R {
         public static final int hello_world_droid=0x7f050003;
         public static final int question=0x7f050006;
         public static final int title_activity_droid=0x7f050005;
+        public static final int ts_history=0x7f050007;
     }
     public static final class style {
         /** 
--- src/com/example/myfirstapp/DroidActivity.java
+++ src/com/example/myfirstapp/DroidActivity.java
@@ -1,6 +1,13 @@
 package com.example.myfirstapp;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -11,8 +18,13 @@ import android.widget.TextView;
 
 public class DroidActivity extends Activity {
 
+	private final String TIMESTAMP_FILENAME = "ts_file";
+	private final int TIMESTAMP_LENGTH_BYTES = 19;
+	private final String CHAR_SET = "UTF-8";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// Always calls the superclass method first.
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_droid);
@@ -23,16 +35,66 @@ public class DroidActivity extends Activity {
 		// findViewById returns a View if it exists in the layout set by 
 		// setContentView.
 		TextView textView = (TextView) findViewById(R.id.intent_message);
-		Log.d("textSize",Float.toString(textView.getTextSize()));
-		textView.setTextSize(24);
+		textView.setTextSize(18);
 		textView.setText(message);
 		textView.setTextColor(Color.RED);
 
+		// Read and display timestamp log
+		String last_ts = readTimestampsFromFile();
+		TextView tsTextView = (TextView) findViewById(R.id.timestamp_history);
+		tsTextView.setText("This activity was last opened at " + last_ts);
+		
+		// Log this entry
+		writeTimestampToFile();
 		
 		// Show the Up button in the action bar.
 		setupActionBar();
 	}
+	
+	private String readTimestampsFromFile() {
+		// Read in previous entries
+		FileInputStream inputStream;
+		try {
+			inputStream = openFileInput(TIMESTAMP_FILENAME);
+			int avail_bytes = inputStream.available();
+			Log.d("avail bytes", Integer.toString(avail_bytes));
+			
+			try {
+				byte[] buffer = new byte[avail_bytes];
+				inputStream.read(buffer);
+				String buffer_str = new String(buffer, CHAR_SET);
+				Log.d("last write", buffer_str.substring(avail_bytes -TIMESTAMP_LENGTH_BYTES));
+				
+				for (int i = 0; i < avail_bytes; i += TIMESTAMP_LENGTH_BYTES) {
+					Log.d("dates", buffer_str.substring(i,i+TIMESTAMP_LENGTH_BYTES));
+				}
+				return buffer_str.substring(avail_bytes -TIMESTAMP_LENGTH_BYTES);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "ERROR";
+	}
 
+	private void writeTimestampToFile() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss", Locale.US);
+		String currentDateandTime = sdf.format(new Date());
+		FileOutputStream outputStream;
+		
+		try {
+			outputStream = openFileOutput(TIMESTAMP_FILENAME, Context.MODE_APPEND);
+			outputStream.write(currentDateandTime.getBytes(CHAR_SET));
+			outputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	/**
 	 * Set up the {@link android.app.ActionBar}.
 	 */
