58a229f6171023e0bd0d99d9b1fb03f595d874fb
Ravi Trivedi <ravi.trivedi3@gmail.com>
2014-02-25 00:09:08 -0800

    Minor UI changes to prevent vomit.

--- gen/com/example/myfirstapp/R.java
+++ gen/com/example/myfirstapp/R.java
@@ -55,9 +55,9 @@ public final class R {
         public static final int action_mindcipher=0x7f050008;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
         public static final int hello_world_droid=0x7f050003;
         public static final int question=0x7f050006;
+        public static final int random_puzzle=0x7f050002;
         public static final int title_activity_droid=0x7f050005;
         public static final int title_activity_mindcipher=0x7f050009;
         public static final int title_activity_puzzle=0x7f05000a;
